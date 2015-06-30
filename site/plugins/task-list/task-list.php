88e15d1a4365a6b90372e73cb0608fd3c3e2a93c
Dennis Frank <github@dennisfrank.info>
2015-06-30 17:26:37 +0200

    Add disabled attribute to checkboxes
    because these checkboxes are not meant to be clicked

--- site/plugins/task-list/task-list.php
+++ site/plugins/task-list/task-list.php
@@ -1,8 +1,8 @@
 <?php
 
 function convert_tasks($text) {
-	$text = preg_replace('/^\s*<li>\s*(<p>)?\s*\[x\]/mi', '<li style="list-style-type:none;">$1<input type="checkbox" checked>', $text);
-	$text = preg_replace('/^\s*<li>\s*\[ \]/mi', '<li style="list-style-type:none;"><input type="checkbox">', $text);
+	$text = preg_replace('/^\s*<li>\s*(<p>)?\s*\[x\]/mi', '<li style="list-style-type:none;">$1<input type="checkbox" disabled checked>', $text);
+	$text = preg_replace('/^\s*<li>\s*\[ \]/mi', '<li style="list-style-type:none;"><input type="checkbox" disabled>', $text);
 	return $text;
 
 }
